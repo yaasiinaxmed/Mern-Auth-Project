80c39eca710337ceff8d1dc60c2bd4ba32665f70
yaasiinaxmed <yaskassoy@gmail.com>
2023-10-16 08:25:24 +0300

    add middleware and a function handle errors

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,11 +6,13 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    
     email: {
         type: String,
         required: true,
         unique: true,
     },
+
     password: {
         type: String,
         required: true,
